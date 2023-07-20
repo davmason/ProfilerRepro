421cc2bfb406fe0ecf1c4dac0b996c10fe83017e
David Mason <davmason@microsoft.com>
2023-07-19 23:25:20 -0700

    changes

--- build.sh
+++ build.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-[ -z "${BuildOS:-}"      ] && export BuildOS=Linux
-[ -z "${BuildArch:-}"    ] && export BuildArch=x64
-[ -z "${BuildType:-}"    ] && export BuildType=Debug
-
-[ -z "${CORECLR_PATH:-}" ] && export CORECLR_PATH=/git/runtime/src/coreclr
-[ -z "${CORECLR_BIN:-}"  ] && export CORECLR_BIN=/git/runtime/artifacts/bin/coreclr/$BuildOS.$BuildArch.$BuildType
-
-printf '  CORECLR_PATH : %s\n' "$CORECLR_PATH"
-printf '  BuildOS      : %s\n' "$BuildOS"
-printf '  BuildArch    : %s\n' "$BuildArch"
-printf '  BuildType    : %s\n' "$BuildType"
-
 printf '  Building ...'
 
 if [ ! -d "bin/" ]; then
