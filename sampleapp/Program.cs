dd1be18baf2be939d1296a6eaf5106481dabd18f
David Mason <davmason@microsoft.com>
2023-07-20 00:41:24 -0700

    changes

--- sampleapp/Program.cs
+++ sampleapp/Program.cs
@@ -9,14 +9,16 @@ namespace SampleApp
 {
     internal class Test<TKey, TValue>
     {
-        public void GenericMethod(TKey item)
+        public void GenericMethod(TValue value)
         {
-            Console.WriteLine($"Test.GenericMethod with types {typeof(TKey)} {typeof(TValue)}");
+            Console.WriteLine($"Test.GenericMethod with types {typeof(TKey)} {typeof(TValue)} value.GetType={value.GetType()} value={value}");
         }
 
         public void GenericMethodT<T>(T item)
         {
-            Console.WriteLine($"Test.GenericMethodT with types {typeof(TKey)} {typeof(TValue)} {typeof(T)}");
+            var test = new Test<string, T>();
+            // Console.WriteLine($"Test.GenericMethodT with types {typeof(TKey)} {typeof(TValue)} {typeof(T)}");
+            test.GenericMethod(item);
         }
     }
 
@@ -24,13 +26,13 @@ namespace SampleApp
     {
         static void Main(string[] args)
         {
-            new Test<int, double>().GenericMethod(1);
+            // new Test<int, double>().GenericMethod(1);
             new Test<int, double>().GenericMethodT(1.0);
-            new Test<Program, List<int>>().GenericMethod(new Program());
+            // new Test<Program, List<int>>().GenericMethod(new Program());
             new Test<Program, List<int>>().GenericMethodT(new List<int>());
-            new Test<int, int>().GenericMethod(1);
+            // new Test<int, int>().GenericMethod(1);
             new Test<int, int>().GenericMethodT(1);
-            new Test<List<int>, List<int>>().GenericMethod(new List<int>());
+            // new Test<List<int>, List<int>>().GenericMethod(new List<int>());
             new Test<List<int>, List<int>>().GenericMethodT(new List<int>());
         }
     }
